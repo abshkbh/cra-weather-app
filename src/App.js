948649907a9276af22df3dc02e8f15852536a7b2
Abhishek Bhardwaj <abshkbh@gmail.com>
2023-03-19 14:59:48 -0700

    Add logic for conditional rendering

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ class App extends React.Component {
     super(props)
     this.state = {
       city: "",
+      wait_for_input: true,
     }
 
     this.updateInput = this.updateInput.bind(this)
@@ -30,19 +31,32 @@ class App extends React.Component {
   // Get the weather on a button click.
   handleGetWeather() {
     console.log("Get Weather for city: " + this.state.city)
+    this.setState({
+      wait_for_input: false
+    })
   }
 
   render() {
-    return (
-      < div className="App" >
-        <h1>Welcome To Weather App</h1>
-        <label>{Constants.CITY_INPUT_LABEL}</label>
-        <input id={Constants.CITY_INPUT_ID} onChange={this.updateInput} />
-        <button onClick={this.handleGetWeather}>
-          {Constants.GET_WEATHER_BUTTON_LABEL}
-        </button>
-      </div >
-    );
+    if (this.state.wait_for_input) {
+      // Render the input form
+      return (
+        <div className="App">
+          <h1>Welcome To Weather App</h1>
+          <label>{Constants.CITY_INPUT_LABEL}</label>
+          <input id={Constants.CITY_INPUT_ID} onChange={this.updateInput} />
+          <button onClick={this.handleGetWeather}>
+            {Constants.GET_WEATHER_BUTTON_LABEL}
+          </button>
+        </div>
+      );
+    } else {
+      // Render the weather information
+      return (
+        <div className="App">
+          <h1>Weather for {this.state.city}</h1>
+        </div>
+      );
+    }
   }
 }
 
