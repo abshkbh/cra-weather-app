d7350292263adc692ae7b5c7248b0d1e86f51383
Abhishek Bhardwaj <abshkbh@gmail.com>
2023-03-19 14:48:47 -0700

    City name and button click working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
 import React from 'react';
+import Constants from './Constants';
 
 // The parent component for the app.
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      city: "",
+    }
+
+    this.updateInput = this.updateInput.bind(this)
+    this.handleGetWeather = this.handleGetWeather.bind(this)
+  }
+
+  // Updates all the text input values.
+  updateInput(e) {
+    const value = e.target.value
+    const id = e.target.id
+
+    if (id === Constants.CITY_INPUT_ID) {
+      this.setState({
+        city: value
+      })
+    } else {
+      console.log("Unrecognized input field:" + id)
+    }
+  }
+
+  // Get the weather on a button click.
+  handleGetWeather() {
+    console.log("Get Weather for city: " + this.state.city)
+  }
+
   render() {
     return (
       < div className="App" >
         <h1>Welcome To Weather App</h1>
+        <label>{Constants.CITY_INPUT_LABEL}</label>
+        <input id={Constants.CITY_INPUT_ID} onChange={this.updateInput} />
+        <button onClick={this.handleGetWeather}>
+          {Constants.GET_WEATHER_BUTTON_LABEL}
+        </button>
       </div >
     );
   }
---
+++ src/Constants.js
@@ -0,0 +1,12 @@
+const Constants = {
+    // Label for the city field.
+    CITY_INPUT_LABEL: "Enter City",
+
+    // Id for the city input field.
+    CITY_INPUT_ID: "city",
+
+    // Label for the get weather button field.
+    GET_WEATHER_BUTTON_LABEL: "Get Weather",
+};
+
+export default Constants;
