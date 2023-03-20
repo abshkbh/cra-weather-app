e4a22366f35d67e29f6ba34b2fe8582bd7bcceef
Abhishek Bhardwaj <abshkbh@gmail.com>
2023-03-19 17:32:39 -0700

    Successfully showed weather data

--- src/Weather.js
+++ src/Weather.js
@@ -15,13 +15,14 @@ class Weather extends React.Component {
     this.state = {
       loading_weather: true,
       // This will be a list of objects like this [{"date1" : {"min_temp" : min_temp, "max_temp": max_temp}, {"date2" : {"min_temp" : min_temp, "max_temp": max_temp}]
-      weather: [],
+      weather_data: [],
     }
 
     this.loadWeather = this.loadWeather.bind(this)
   }
 
   loadWeather() {
+    // TODO: Modify route for this.props.city.
     let route_to_fetch = "https://api.open-meteo.com/v1/forecast?latitude=30.06&longitude=31.25&daily=temperature_2m_max,temperature_2m_min&start_date=2023-03-19&end_date=2023-03-26&timezone=America%2FLos_Angeles"
     console.log('Posting to: ' + route_to_fetch)
     fetch(route_to_fetch,
@@ -41,10 +42,15 @@ class Weather extends React.Component {
         console.log("Weather API entries: ", num_entries)
         for (let i = 0; i < num_entries; i++) {
           let entry_date = data.daily.time[i];
-          let entry = { [entry_date]: { "min_temp": data.daily.temperature_2m_min[i], "max_temp": data.daily.temperature_2m_max[i] } };
+          let entry = { "date": entry_date, "min_temp": data.daily.temperature_2m_min[i], "max_temp": data.daily.temperature_2m_max[i] };
           weather_data.push(entry)
         }
         console.log("Weather Data ", weather_data)
+        this.setState({
+          loading_weather: false,
+
+          weather_data: weather_data,
+        })
       }
       )
       .catch(error => {
@@ -65,7 +71,15 @@ class Weather extends React.Component {
     }
 
     return (
-      <h1>TODO</h1>
+      <ul>
+        {this.state.weather_data.map((obj, index) => (
+          <li key={index}>
+            <p>Date: {obj.date}</p>
+            <p>Min Temp: {obj.min_temp}</p>
+            <p>Max Temp: {obj.max_temp}</p>
+          </li>
+        ))}
+      </ul>
     );
   }
 
