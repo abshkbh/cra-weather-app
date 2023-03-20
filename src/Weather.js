3892ce8054e35d2d9bfb1f485c15d3303eb59f79
Abhishek Bhardwaj <abshkbh@gmail.com>
2023-03-19 17:23:24 -0700

    Made successful call to Weather API

--- src/Weather.js
+++ src/Weather.js
@@ -1,12 +1,60 @@
 import React from 'react';
 
+// Takes in a HTTP |response| and throws an error if it's not 200 else returns |response| as is.
+function handleFetchErrors(response) {
+  if (!response.ok) {
+    throw Error(response.statusText);
+  }
+  return response;
+}
+
 // Shows the weather for a city.
 class Weather extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
       loading_weather: true,
+      // This will be a list of objects like this [{"date1" : {"min_temp" : min_temp, "max_temp": max_temp}, {"date2" : {"min_temp" : min_temp, "max_temp": max_temp}]
+      weather: [],
     }
+
+    this.loadWeather = this.loadWeather.bind(this)
+  }
+
+  loadWeather() {
+    let route_to_fetch = "https://api.open-meteo.com/v1/forecast?latitude=30.06&longitude=31.25&daily=temperature_2m_max,temperature_2m_min&start_date=2023-03-19&end_date=2023-03-26&timezone=America%2FLos_Angeles"
+    console.log('Posting to: ' + route_to_fetch)
+    fetch(route_to_fetch,
+      {
+        method: 'GET',
+      }
+    ).then(handleFetchErrors)
+      .then(response => {
+        console.log("Weather API Response: ", response)
+        return response.json()
+      }
+      )
+      .then(data => {
+        console.log("Weather API Data: ", data)
+        const num_entries = data.daily.time.length
+        let weather_data = []
+        console.log("Weather API entries: ", num_entries)
+        for (let i = 0; i < num_entries; i++) {
+          let entry_date = data.daily.time[i];
+          let entry = { [entry_date]: { "min_temp": data.daily.temperature_2m_min[i], "max_temp": data.daily.temperature_2m_max[i] } };
+          weather_data.push(entry)
+        }
+        console.log("Weather Data ", weather_data)
+      }
+      )
+      .catch(error => {
+        console.log("Weather API Error: ", error)
+      })
+
+  }
+
+  componentDidMount() {
+    this.loadWeather()
   }
 
   render() {
